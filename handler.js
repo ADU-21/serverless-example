450117bb47b71d7c8cd7e1ffa0b701573dc76a6d
yiddu <yiddu@thoughtworks.com>
2017-11-21 00:02:37 +0800

    remove one api

--- handler.js
+++ handler.js
@@ -4,29 +4,9 @@ module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'v1.0',
-      // input: event,
+      version: 'v1.0',
     }),
   };
 
   callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
-
-
-module.exports.imageResize = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'this is new version for Resize Your Image',
-      // input: event,
-    }),
-  };
-
-  callback(null, event);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
