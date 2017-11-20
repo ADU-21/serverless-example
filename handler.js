284bd3d803b39d71d5a30e9d65afbe7e285a7486
yiddu <yiddu@thoughtworks.com>
2017-11-20 23:54:36 +0800

    remove alias plugin

--- handler.js
+++ handler.js
@@ -25,7 +25,7 @@ module.exports.imageResize = (event, context, callback) => {
     }),
   };
 
-  callback(null, response);
+  callback(null, event);
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
