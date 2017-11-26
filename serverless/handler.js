3d162b8aa52552c096a0f9bc845a2dd485d4c6a9
yiddu <yiddu@thoughtworks.com>
2017-11-26 22:53:44 +0800

    commit final for a while

--- serverless/handler.js
+++ serverless/handler.js
@@ -4,7 +4,7 @@ module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      version: 'v1.3',
+      event: event,
     }),
   };
 
